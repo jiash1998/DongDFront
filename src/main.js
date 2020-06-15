8ec23086c83752edcd89c771d68dbb3ebd254c17
jiash1998 <1459538273@qq.com>
2020-06-15 16:12:16 +0800

    initpartone

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//引入element npm i element-ui -S
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+//引入sass 
+//npm install --save-dev sass-loader
+//npm install --save-dev node-sass
+
+//引入querystring POST请求
+import qs from "querystring";
+
+//引入axios cnpm install axios/cnpm install vue-axios
+import axios from "axios"; //axios
+import VueAxios from "vue-axios"; //axios
+
+
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false
 
 new Vue({
